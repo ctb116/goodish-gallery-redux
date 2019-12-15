a34ca569edc386bba0b60cac4e1461426ea6bb3f
Cathy Bradley (ctb116) <catherinetybradley@gmail.com>
2019-12-15 13:36:51 -0800

    extend banner component to prevent rerendering

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import { getImages } from "../services/imageService";
 import "../styles/banner.css";
 
-const Banner = () => {
-  let image = getImages();
-  let rand = image[Math.floor(Math.random() * image.length)];
-  return (
-    <div className="banner-crop">
-      <img src={process.env.PUBLIC_URL + rand.src} alt="featured drawing"></img>
-    </div>
-  );
-};
+class Banner extends Component {
+  state = {
+    rand: getImages()[Math.floor(Math.random() * getImages().length)]
+  };
+  render() {
+    return (
+      <div className="banner-crop">
+        <img
+          src={process.env.PUBLIC_URL + this.state.rand.src}
+          alt="featured drawing"
+        ></img>
+      </div>
+    );
+  }
+}
 
 export default Banner;
